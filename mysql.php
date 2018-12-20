06c2a22c629510390e9e94a0b99be4712bbaba02
BeaumonteT <44506046+BeaumonteT@users.noreply.github.com>
2018-12-20 09:09:25 -0800

    mysqlfirst

--- mysql.php
+++ mysql.php
@@ -5,11 +5,4 @@
 	$pass = "";
 	$db = "c9";
 	$port = 3306;
-
-	$connection = mysqli connect($host, $user, $pass, $db, $port)or die(mysql error());
-	$query = "SELECT * FROM users";
-	$result = mysqli query($connection, $query);
-
-	while ($row = mysqli fetch assoc($result)) {
-		echo "The ID is: " . $row['id'] . " and the Username is: " . $row['username'];
-	}
+?>
