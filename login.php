8d2cac2c6d86587b6e241fe9cb1a63c2da71ff87
BeaumonteT <44506046+BeaumonteT@users.noreply.github.com>
2018-12-20 09:22:04 -0800

    sql

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
+login.php
 <?php
-
 	$host = "127.0.0.1";
 	$user = "beaut";
 	$pass = "";
-	$db = "c9";
+	$db = "sample_db";
 	$port = 3306;
 ?>
---
+++ phptest2.php
@@ -0,0 +1,17 @@
+<?php
+		include_once 'formtest.html';
+		require_once 'login.php';
+		$conn = mysqli_connect($host, $user, $pass, $db, $port);
+		if($conn->connect_error) die($conn->connect_error);
+		$option = isset($_POST['myTest']) ? $_POST['myTest'] : false;
+		if ($option){
+			$testField = $_POST['myTest'];
+			$query = 	select ". $testField . " fromusers;";
+			$result = mysqli_query($conn, $query);
+			while ($row = mysqli_fetch_assoc($result)) {
+				echo $row[$testfield] . "<br>";
+			}
+		}
+		$result->close();
+		$conn->close();
+?>
