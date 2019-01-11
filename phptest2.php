d162ab0b48057303ba41cb32bd606900acd8d5e6
BeaumonteT <44506046+BeaumonteT@users.noreply.github.com>
2019-01-11 09:58:35 -0800

    fixing oen thihng

--- phptest2.php
+++ phptest2.php
@@ -6,7 +6,7 @@
 		$result = mysqli_query($connection, $query);
 		echo "<table><tr><th>Sport</th><th>Season</th><th>Location</th></tr>";
 		while ($row = mysqli_fetch_assoc($result)) {
-			echo "<tr><td>".$row["Sport"]."</td><td>".$row["Season"]."</td><td>".$row["Location"]"</td></tr>";
+			echo "<tr><td>".$row["Sport"]."</td><td>".$row["Season"]."</td><td>".$row["Location"]."</td></tr>";
 	}
 echo "</table>"
 ?>
