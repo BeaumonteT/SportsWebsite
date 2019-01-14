98e2277bb387b1eb72ce3666e5ad24296d84b78e
BeaumonteT <44506046+BeaumonteT@users.noreply.github.com>
2019-01-14 10:09:06 -0800

    sql1

--- phptest2.php
+++ phptest2.php
@@ -5,7 +5,7 @@
 		$query = "SELECT * FROM athletics";
 		$result = mysqli_query($connection, $query);
 		echo "<table><tr><th>Sport</th><th>Season</th><th>Location</th></tr>";
-		$row = mysqli_fetch_assoc($result) {
+		echo $row = mysqli_fetch_assoc($result) {
 			echo "<tr><td>".$row["Sport"]."</td><td>".$row["Season"]."</td><td>".$row["Location"]."</td></tr>";
 	}
 echo "</table>"
